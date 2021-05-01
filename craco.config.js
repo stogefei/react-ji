cc451b367a17abc0649c6c4819e8230e257dbd99
zhuaofei <2353536563@qq.com>
2021-05-01 11:14:55 +0800

    fix: 添加文件

---
+++ craco.config.js
@@ -0,0 +1,23 @@
+const CracoLessPlugin = require('craco-less');
+
+module.exports = {
+    babel: {
+        plugins: [
+            ['import', { libraryName: 'antd', style: true }],
+            ['@babel/plugin-proposal-decorators', { legacy: true }]
+        ]
+    },
+    plugins: [
+        {
+            plugin: CracoLessPlugin,
+            options: {
+                lessLoaderOptions: {
+                    lessOptions: {
+                        modifyVars: { '@primary-color': '#1DA57A' },
+                        javascriptEnabled: true,
+                    },
+                },
+            },
+        },
+    ],
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import "./App.css";
+import { Button } from 'antd';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <Button type="primary">Button</Button>
         <span>w</span>
       </header>
     </div>
